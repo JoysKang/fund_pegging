54f39a48644e2ae37858cc9b886e42182392763a
joys <joys.karen@foxmail.com>
2022-01-01 20:19:06 +0800

    沪深300、中证500、创业板 14:30 数据获取

--- trend.py
+++ trend.py
@@ -11,33 +11,40 @@ import json
 import datetime
 
 import akshare as ak
+import requests
+
 
 # https://cloud.tencent.com/developer/article/1429693  学习资料
 
 
-def get_data():
+def get_data(symbol=None):
     """获取前 22 个交易的 14:30 数据"""
-
-    import requests
+    if symbol is None:
+        return 0
 
     params = (
-        ('symbol', 'sz399300'),
+        ('symbol', symbol),
         ('scale', '30'),
         ('ma', 'no'),
         ('datalen', '1023'),
     )
 
     response = requests.get(
-        'https://quotes.sina.cn/cn/api/jsonp_v2.php/var%20_sz399300_30_1638272168085=/CN_MarketDataService.getKLineData',
+        f'https://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol={symbol}&scale=30&ma=&datalen=220',
         params=params)
-    start = response.text.find('=(') + 2
-    end = len(response.text) - 2
-    data = json.loads(response.text[start:end])
+    data = response.json()
     data = [d for d in data if "14:30:00" in d['day']]
-    # print(data)
-    print(data[-22]['close'])
+    close = data[-22]['close']
+    print(f'{symbol} 前 22 个交易日的 14:30 收盘价为 {close}')
+
+
+def main():
+    """主函数"""
+    get_data('sz399300')    # 沪深300
+    get_data('sz399905')    # 中证500
+    get_data('sz399006')    # 创业板
 
 
 if __name__ == '__main__':
-    get_data()
+    main()
 
